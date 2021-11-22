f929368783263bad6d576216ec9f12d2f0fc9b02
Dis <2632699773@qqcom>
2021-11-22 15:56:07 +0800

    创建User类
    Signed-off-by: Dis <2632699773@qqcom>

---
+++ Server/src/main/java/top/gytf/family/server/dao/User.java
@@ -0,0 +1,15 @@
+package top.gytf.family.server.dao;
+
+/**
+ * Project:     IntelliJ IDEA
+ * ClassName:   User
+ * Description: 用户DAO
+ * CreateDate:  2021/11/22 15:49
+ * ------------------------------------------------------------------------------------------
+ *
+ * @author user
+ * @version V1.0
+ */
+public class User {
+    private final static String TAG = User.class.getName();
+}
