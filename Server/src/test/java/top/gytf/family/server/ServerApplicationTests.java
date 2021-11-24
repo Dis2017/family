e6bc3ee6e513d822da0c314c76b5c10607f724b8
Dis <2632699773@qqcom>
2021-11-25 00:14:05 +0800

    测试样例
    Signed-off-by: Dis <2632699773@qqcom>

--- Server/src/test/java/top/gytf/family/server/ServerApplicationTests.java
+++ Server/src/test/java/top/gytf/family/server/ServerApplicationTests.java
@@ -1,25 +1,24 @@
 package top.gytf.family.server;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import top.gytf.family.server.config.security.SecurityConfig;
 import top.gytf.family.server.entity.User;
 import top.gytf.family.server.mapper.UserMapper;
 
-import java.util.List;
-
 @SpringBootTest
-@Slf4j
 class ServerApplicationTests {
 
     @Autowired
-    private SecurityConfig securityConfig;
+    private UserMapper userMapper;
 
     @Test
     void contextLoads() {
-        log.debug("point");
+        User userDao = User.builder()
+                .name("Test")
+                .password("cnmd")
+                .build();
+        userMapper.insert(userDao);
     }
 
 }
